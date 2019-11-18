753d6e798e82d0c5f0c5e059f7e717e664dfe41f
alex <xiayao.zeng@gmail.com>
2019-11-19 00:05:00 +0800

    feat: add todos

--- index.js
+++ index.js
@@ -27,6 +27,9 @@ http.get(`${baseUrl}${swaggerDocPath}`, res => {
   res.on('end', () => {
     console.log('ended');
     const { paths, definitions } = JSON.parse(result);
+    //  TODO: 增加对paths中各方法的解析
+    //  TODO: 增加对方法参数的解析
+    //  TODO: 增加对definitions的解析
     generateFiles(paths);
   });
   res.on('close', () => {
