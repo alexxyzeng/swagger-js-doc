702789448a238027e8f3d272eeb4e2d6821fc215
alex <xiayao.zeng@gmail.com>
2019-12-01 13:59:40 +0800

    feat: add parse definition for schema

--- helpers/parseParamters.js
+++ helpers/parseParamters.js
@@ -8,60 +8,80 @@ const {
 function parseParams(api, definitions) {
   const { parameters } = api;
   const parsedParameters = { body: [], path: [], query: [] };
+  let result = {};
   parameters.forEach(parameter => {
-    const {
-      name,
-      description,
-      required,
-      type,
-      items,
-      in: paramIn,
-      schema
-    } = parameter;
-
-    if (parsedParameters[paramIn]) {
-      const { parsedType } = parseParamType(type, items);
-      // console.log(type);
-      // console.log(schema);
-      // console.log('============');
-      if (isValidDefinitionType(schema)) {
-        const definitionType = parseDefinitionType(schema);
-        //  FIXME: 循环有相同类型，导致有递归越界
-        const notOkList = ['设施分类', '权限表'];
-        if (notOkList.includes(definitionType)) {
-          fs.writeFileSync('./test.js', JSON.stringify(api));
-          return;
-        }
-        // TODO: 增加对参数类型的整理
-        // const definition = parseDefinition(definitionType, definitions);
-      }
-      parsedParameters[paramIn].push({
-        name,
-        description,
-        required,
-        type: parsedType
-      });
+    const { in: paramIn, schema } = parameter;
+    if (!parsedParameters[paramIn]) {
+      return;
     }
+    // const param = parseParam(parameter);
+    let param = null;
+    if (isValidDefinitionType(schema)) {
+      const definitionType = parseDefinitionType(schema);
+      const notOkList = ['设施分类', '权限表'];
+      if (notOkList.includes(definitionType)) {
+        return;
+      }
+      const definition = parseDefinition(definitionType, definitions);
+      param = parseParamType(definition);
+      // console.log(definition);
+    } else {
+      param = parseParamType(parameter);
+    }
+    // console.log(param);
+    // console.log('====================================');
+    // console.log(param);
+    // console.log('====================================');
   });
-  // console.log('============');
-  // console.dir(parsedParameters);
-  // console.log('============');
 }
 
 // integer/strring/array/object/enum/boolean
-function parseParamType(type, items) {
+function parseParamType(param) {
+  if (!param) {
+    return null;
+  }
+  const { type, required, items, description } = param;
+
   if (type === 'integer' || type === 'float' || type === 'double') {
-    return { type: 'number' };
+    return { type: 'number', description, required };
   }
   if (type === 'array') {
-    const { type: itemType } = items;
-    const parsedItemType = parseParamType(itemType);
-    return { type, format: parsedItemType };
+    // const { type: itemType } = items;
+    // TODO: 增加对复杂数组类型的解析
+    console.log('====================================');
+    console.log(items);
+    console.log('====================================');
+    // const parsedItemType = parseParamType(itemType);
+    // return { type, format: parsedItemType, reuired };
   }
   // TODO: 解析enum
   // TODO: 解析object
-  return { type };
+  if (type === 'object') {
+    const { properties, required = {} } = param;
+    const requireStatus = Object.values(required).reduce((a, b) => {
+      return (a[b] = 1);
+    }, {});
+    let result = {};
+    for (let i in properties) {
+      const property = properties[i];
+      const propertyFinal = {
+        ...property,
+        required: requireStatus[i] || false
+      };
+      result[i] = parseParamType(propertyFinal);
+    }
+    // console.log('====================================');
+    // console.log(properties);
+    // console.log('----------------------------');
+    // console.log(result);
+    // console.log('====================================');
+    // fs.appendFileSync('test.js', JSON.stringify(result));
+    return result;
+  }
+  return { type, description };
 }
+
+function parseParam(param) {}
 
 module.exports = {
   parseParams
